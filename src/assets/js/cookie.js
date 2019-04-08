9ae77dbc202e17110ea94dd9e966a389fe774b0d
Custer <525966315@qq.com>
2019-04-08 20:40:51 +0800

    cookie time

--- src/assets/js/cookie.js
+++ src/assets/js/cookie.js
@@ -1,6 +1,7 @@
 export function setCookie(name, value) {
-  var Days = 30;
+  var Days = 1;
   var exp = new Date();
+  // exp.setTime(exp.getTime() + 60 * 1000);
   exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
   document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
 }
